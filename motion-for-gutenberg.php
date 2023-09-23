749f3b3fc0334c375e617468f16a11dab2fb48fa
Denis Dumont <denis.dumont09@gmail.com>
2023-09-23 23:13:15 +0200

    update version

--- motion-for-gutenberg.php
+++ motion-for-gutenberg.php
@@ -12,7 +12,7 @@ use MotionForWP\Translations;
  * Description:       Put the magic of motion in your Gutenberg blocks.
  * Requires at least: 5.7
  * Requires PHP:      7.4
- * Version:           0.9.0
+ * Version:           0.9.1
  * Author:            denisdums
  * Author URI:        https://denisdums.com
  * License:           GPL-2.0-or-later
